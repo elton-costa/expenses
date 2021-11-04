47b2536859a5c50c5710a4f473f1af86f7981803
elton-costa <tonrc1@hotmail.com>
2021-11-04 08:50:45 -0300

    Add Transactions

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -29,8 +33,10 @@ class TransactionForm extends StatelessWidget {
                 style: const TextStyle(color: Colors.white),
               ),
               onPressed: () {
-                print(titleController.text);
-                print(valueController.text);
+                final title = titleController.text;
+                final value = double.tryParse(valueController.text) ?? 0.0;
+
+                onSubmit(title, value);
               },
               style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(Colors.purple),
